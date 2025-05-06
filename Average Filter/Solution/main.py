57e71fa90243326900d2599d5db9fe901f928ef9
Pedro Vitor Pereira <PedroVitorPereira@MacBook-Pro-de-Pedro-Vitor-Pereira.local>
2025-05-05 23:27:24 -0300

    Activity 02: Implemented average filter with integral image and image saving

--- Average Filter/Solution/main.py
+++ Average Filter/Solution/main.py
@@ -6,6 +6,9 @@ Challenge:
 '''
 
 import sys
+import time
+
+import cv2
 import cv2 as cv
 import numpy as np
 
@@ -17,35 +20,33 @@ class HandleImage:
     def __init__(self):
         pass
 
-    def showImage(self, image, titleWindow):
-        cv.imshow(titleWindow, (image * 255).astype('uint8'))
+    def showImage(self, img, titleWindow):
+        cv.imshow(titleWindow, (img * 255).astype(np.uint8))
         cv.waitKey(0)
         cv.destroyAllWindows()
 
-    def SaveImage(self):
-        pass
+    def SaveImage(self, img, titleWindow):
+        img = (img * 255).astype(np.uint8)
+        cv.imwrite(titleWindow, img)
+        time.sleep(0.1)
 
 
 class ImageProcessor:
     def __init__(self):
         pass
 
-    def Makebiniarizaed(self, image):
-        binarizedImage = np.where(image >= THRESHOLD, 1, 0).astype('uint16')
-        return binarizedImage
-
-    def ApplyAverageFilterBasic(self, image, kernel_size):
+    def ApplyAverageFilterBasic(self, img, kernel_size):
 
         offset = kernel_size // 2
-        img_out = np.zeros_like(image)
+        img_out = np.zeros_like(img)
 
-        for row in range(image.shape[0]):
-            for column in range(image.shape[1]):
+        for row in range(img.shape[0]):
+            for column in range(img.shape[1]):
                 sum = 0
                 for krow in range(row - offset, row + offset + 1):
                     for kcolumn in range(column - offset, column + offset + 1):
-                        if 0 <= krow < image.shape[0] and 0 <= kcolumn < image.shape[1]:
-                            sum += image[krow][kcolumn]
+                        if 0 <= krow < img.shape[0] and 0 <= kcolumn < img.shape[1]:
+                            sum += img[krow][kcolumn]
                         else:
                             sum += 0
                 img_out[row][column] = sum / (kernel_size ** 2)
@@ -111,39 +112,34 @@ class ImageProcessor:
         offset_vertical = height // 2
         offset_horizontal = width // 2
 
-        img_pad = np.pad(img, ((1, 1), (1, 1)), 'constant')
-        img_integral = np.zeros_like(img_pad,dtype=np.float32)
-        img_out = np.zeros_like(img,dtype=np.float32)
+        img = img.astype(np.float32)
+        img_pad = np.pad(img, ((1, 1), (1, 1)), 'constant').astype(np.float32)
+        img_integral = np.zeros_like(img_pad,dtype=np.float64)
+        img_out = np.zeros_like(img_integral,dtype=np.float64)
 
-        #Calculing integral image
         for row in range(1, img_pad.shape[0] ):
-            sum = np.sum(img_pad[row, 0:width])
             for column in range(1, img_pad.shape[1]):
                 img_integral[row][column] = (img_pad[row, column] +
-                                                           img_integral[row - 1, column] +
-                                                           img_integral[row, column - 1] +
-                                                           img_integral[row - 1, column - 1])
-        #Blur image
+                                             img_integral[row - 1, column] +
+                                             img_integral[row, column - 1] -
+                                             img_integral[row - 1, column - 1])
+
         for row in range(img_integral.shape[0]):
+            sum = 0
             for column in range(img_integral.shape[1]):
-                sum = 0
-                r1 = row + 1 - offset_vertical
-                r2 = row + 1 + offset_vertical
-                c1 = column + 1 - offset_horizontal
-                c2 = column + 1 + offset_horizontal
-
-                r1 = max(r1, 0)
-                c1 = max(c1, 0)
-                r2 = min(r2, img_integral.shape[0] - 1)
-                c2 = min(c2, img_integral.shape[1] - 1)
-
-                sum = (img_integral[r2, c2] - img_integral[r1 - 1, c2] - img_integral[r2, c1 - 1] + img_integral[r1 - 1, c1 - 1])
-                img_out[row][column] = sum / (height * width)
+                if ((row + 1 + offset_vertical )< img_integral.shape[0] and (column + 1 + offset_horizontal) < img_integral.shape[1] and (row + 1 - offset_vertical - 1) >= 0 and (column + 1 + offset_horizontal -1 ) >= 0 ):
+                    sum = (img_integral[row + 1 + offset_vertical, column + 1 + offset_horizontal]
+                           - img_integral[row + 1 - offset_vertical - 1, column + 1 + offset_horizontal]
+                           - img_integral[row + 1 + offset_vertical, column + 1 - offset_horizontal - 1]
+                           + img_integral[row + 1 - offset_vertical - 1, column + 1 - offset_horizontal - 1])
+                    img_out[row][column] = sum / (height * width)
 
         return img_out
 
 
 def main():
+    height = 5
+    width = 5
     imgProcessor = ImageProcessor()
     handleImage = HandleImage()
 
@@ -151,25 +147,38 @@ def main():
     img = img.astype(np.float32) / 255
 
     if img is None:
-        print("Could not load image")
+        print("Could not load img")
         sys.exit()
 
-    #img_binarized = imgProcessor.Makebiniarizaed(img)
+    img_blur_basic = imgProcessor.ApplyAverageFilterBasic(img, 5)
 
-    #img_blur_basic = imgProcessor.ApplyAverageFilterBasic(img, 5)
+    handleImage.showImage(img_blur_basic, "Image Blur with the basic implemmentation")
 
-    #img_blur_separable_basic = imgProcessor.ApplySeparableAverageFilterBasic(img, 3, 3)
+    handleImage.SaveImage(img_blur_basic, "Blur_Basic_" + str(height) +"_"+ str(width) +".png")
+
+    #img_blur_separable_basic = imgProcessor.ApplySeparableAverageFilterBasic(img, height, width)
 
     #handleImage.showImage(img_blur_separable_basic, "Image Blur Basic")
 
-    #img_blur_separable_smart = imgProcessor.ApplySeparableAverageFilterSmart(img, 3, 3)
+    #handleImage.SaveImage(img_blur_basic, "Blur_Separable_Basic_" + str(height) + "_" + str(width) + ".png")
+
+    #img_blur_separable_smart = imgProcessor.ApplySeparableAverageFilterSmart(img, height, width)
 
     #handleImage.showImage(img_blur_separable_smart, "Image Blur Smart")
 
-    image_blur_integral_image = imgProcessor.ApplyIntegralImage(img, 3, 3)
+    #handleImage.SaveImage(img_blur_basic, "Blur_Separable_Smart_" + str(height) + "_" + str(width) + ".png")
 
-    handleImage.showImage(image_blur_integral_image, "Image Blur with integral image")
+    #image_blur_integral_image = imgProcessor.ApplyIntegralImage(img, height, width)
 
+    #handleImage.showImage(image_blur_integral_image, "Image Blur with integral img")
+
+    #handleImage.SaveImage(img_blur_basic, "Blur_Integral_Image_" + str(height) + "_" + str(width) + ".png")
+
+    #image_blur_open_cv = cv.blur(img,(height,width))
+
+    #handleImage.showImage(image_blur_open_cv, "Image Blur with OpenCv implementation")
+
+    #handleImage.SaveImage(img_blur_basic, "Image_Blur_Open_CV" + str(height) + "_" + str(width) + ".png")
 
 if __name__ == '__main__':
     main()
